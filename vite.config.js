0f279d42b763a12cb96a8741ac79fdba26e39174
Elliot Voris <elliot@stellar.org>
2023-06-23 10:33:39 -0500

    updating things, how is buffer looking now?
    Signed-off-by: Elliot Voris <elliot@stellar.org>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import inject from '@rollup/plugin-inject';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+    plugins: [
+        sveltekit(),
+    ],
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: 'globalThis'
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true,
+                }),
+            ],
+        },
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                inject({
+                    window: path.resolve('src/lib/window.js'),
+                }),
+            ],
+        },
+    },
+})
